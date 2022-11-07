d2fca70660fa3159c4614fa2e6c7aa36f4a62f5d
Uchchash346 <mru416@gmail.com>
2022-11-07 11:01:34 +0600

    Setup pages

---
+++ src/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1>☹️😢😭</h1>
+            <h1>Sorry! Page Not Found</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
